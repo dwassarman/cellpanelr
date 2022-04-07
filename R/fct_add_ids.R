0b19c9c073d41c4502831e187aa75e3c16d968c3
Douglas Wassarman <dwassar@gmail.com>
2022-04-07 12:13:12 -0700

    change argument to cell_col

--- R/fct_add_ids.R
+++ R/fct_add_ids.R
@@ -8,7 +8,7 @@
 #'    cell-ID pairs.
 #'
 #' @param data Tibble with one column that contains cell line names
-#' @param name_col Name of column containing cell line names
+#' @param cell_col Name of column containing cell line names
 #'
 #' @return Tibble with new column appended named depmap_id
 #' @export
@@ -22,7 +22,7 @@
 #' )
 #'
 #' add_ids(data, "CellLine")
-add_ids <- function(data, name_col) {
+add_ids <- function(data, cell_col) {
   id_df <- dplyr::select(
     cellpanelr::annotations,
     .data[["stripped_name"]],
@@ -30,51 +30,51 @@ add_ids <- function(data, name_col) {
   )
   data %>%
     dplyr::mutate(
-      stripped_name = gsub("[^[:alnum:]]", "", .data[[name_col]]) %>% toupper()
+      stripped_name = gsub("[^[:alnum:]]", "", .data[[cell_col]]) %>% toupper()
     ) %>%
     dplyr::left_join(id_df,
       by = c("stripped_name"),
       suffix = c("", ".depmap")
     ) %>%
     dplyr::select(-.data[["stripped_name"]]) %>%
-    remove_multi_id(name_col) %>%
-    remove_multi_cell(name_col)
+    remove_multi_id(cell_col) %>%
+    remove_multi_cell(cell_col)
 }
 
 #' Remove IDs that map to the same cell line
 #'
 #' @param data Tibble
-#' @param name_col Name of column containing cell line names
+#' @param cell_col Name of column containing cell line names
 #' @param id_col Name of column containing IDs
 #'
 #' @return Tibble with bad IDs changed to NA
 #'
 #' @importFrom rlang :=
-remove_multi_id <- function(data, name_col, id_col = "depmap_id") {
+remove_multi_id <- function(data, cell_col, id_col = "depmap_id") {
   multi_id <- data %>%
-    dplyr::group_by(.data[[name_col]]) %>%
+    dplyr::group_by(.data[[cell_col]]) %>%
     dplyr::summarise(n = dplyr::n_distinct(.data[[id_col]])) %>%
     dplyr::filter(.data[["n"]] > 1)
 
   data %>%
     dplyr::mutate(
-      !!id_col := ifelse(.data[[name_col]] %in% multi_id[[name_col]], NA, .data[[id_col]])
+      !!id_col := ifelse(.data[[cell_col]] %in% multi_id[[cell_col]], NA, .data[[id_col]])
     )
 }
 
 #' Remove IDs that map to more than one cell line
 #'
 #' @param data Tibble
-#' @param name_col Name of column containing cell line names
+#' @param cell_col Name of column containing cell line names
 #' @param id_col Name of column containing IDs
 #'
 #' @return Tibble with bad IDs changed to NA
 #'
 #' @importFrom rlang :=
-remove_multi_cell <- function(data, name_col, id_col = "depmap_id") {
+remove_multi_cell <- function(data, cell_col, id_col = "depmap_id") {
   multi_cell <- data %>%
     dplyr::group_by(.data[[id_col]]) %>%
-    dplyr::summarise(n = dplyr::n_distinct(.data[[name_col]])) %>%
+    dplyr::summarise(n = dplyr::n_distinct(.data[[cell_col]])) %>%
     dplyr::filter(.data[["n"]] > 1)
 
   data %>%
