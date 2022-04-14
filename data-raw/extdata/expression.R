ed751668b9fe00eb040c47e9b31d1e526cfb94ed
Douglas Wassarman <dwassar@gmail.com>
2022-04-14 16:03:25 -0700

    expression data set local in extdata

---
+++ data-raw/extdata/expression.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(vroom)
+
+# Last run on 4/14/2022 with 22Q1 release
+
+# Download data
+expression <- vroom("../DepMap data/22Q1/CCLE_expression.csv")
+
+# Fix column names
+names <- names(expression)
+names[[1]] <- "depmap_id"
+
+fix_names <- function(name) {
+  str_split(name, " ")[[1]][[1]]
+}
+
+names(expression) <- names
+
+# Save file in most compressed RDS format
+saveRDS(expression, "./inst/extdata/expression.rds", compress = "xz")
--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -1,19 +1,23 @@
+library(tidyverse)
+
 # list of unique cell line IDs contained in the gene expression data set
-expression <- depmap::depmap_TPM()
+expression <- depmap::depmap_TPM() %>%
+  dplyr::select(depmap_id, gene_name, rna_expression)
 .exp_ids <- expression$depmap_id %>% unique()
-.exp_short <- expression %>%
-  head(n = 20000)
+.exp_short <- head(expression, n = 20000)
 
-# example data for Shiny app
-# Single value
-.dasatinib_single <- vroom::vroom("../sample_data/dasatinib.tsv") %>%
-  # remove cell lines with no data
-  dplyr::filter(!is.na(auc)) %>%
-  dplyr::rename(area_under_curve = auc) %>%
-  dplyr::select(cell_line, log_ic50, area_under_curve)
+# # UNCOMMENT TO USE SAMPLE DATA
+# # example data for Shiny app
+# # Single value
+# .dasatinib_single <- vroom::vroom("../sample_data/dasatinib.tsv") %>%
+#   # remove cell lines with no data
+#   dplyr::filter(!is.na(auc)) %>%
+#   dplyr::rename(area_under_curve = auc) %>%
+#   dplyr::select(cell_line, log_ic50, area_under_curve)
 
-usethis::use_data(.exp_ids,
-  .dasatinib_single,
+usethis::use_data(
+  .exp_ids,
+  # .dasatinib_single,
   .exp_short,
   internal = TRUE,
   overwrite = TRUE
