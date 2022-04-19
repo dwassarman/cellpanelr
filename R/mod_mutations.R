474b998a21aab199f9b6b7485b09155a28d14bb1
Douglas Wassarman <dwassar@gmail.com>
2022-04-19 12:01:15 -0700

    Hover tooltip on volcano plot

--- R/mod_mutations.R
+++ R/mod_mutations.R
@@ -20,7 +20,8 @@ mod_mutations_ui <- function(id){
         DT::DTOutput(ns("table"))
       ),
       mainPanel(
-        plotOutput(ns("plot")) %>% shinycssloaders::withSpinner()
+        plotOutput(ns("plot"), hover = ns("plot_hover")) %>% shinycssloaders::withSpinner(),
+        uiOutput(ns("hover_info"), style = "pointer-events: none")
       )
     )
   )
@@ -80,7 +81,8 @@ mod_mutations_server <- function(id, rv){
         dplyr::filter(!is.na(.data$p.value)) %>%
         ggplot(aes(x = .data$effect, y = -log10(.data$p.value), color = .data$significant)) +
         geom_point(alpha = 0.4) +
-        xlab("log2(mutant/wildtype)")
+        xlab("log2(mutant/wildtype)") +
+        ylab("-log10(p.value)")
     },
     height = function() {
       0.75 * session$clientData[["output_mutations_1-plot_width"]]
@@ -88,7 +90,42 @@ mod_mutations_server <- function(id, rv){
     res = 96
     )
     
-    
+    # Create tooltip for hovering over points in plot
+    # See here for reference: https://gitlab.com/-/snippets/16220
+    output$hover_info <- renderUI({
+      req(input$plot_hover)
+      hover <- input$plot_hover
+      
+      # Find point near hover
+      df <- gene_cor() %>%
+        dplyr::mutate(
+          p.value = -log10(.data$p.value)
+        )
+      
+      point <- nearPoints(df, xvar = "effect", yvar = "p.value", hover, maxpoints = 1)
+      
+      
+      if (nrow(point) == 0) {
+        return(NULL)
+      }
+      
+      left_px <- hover$coords_css$x
+      top_px <- hover$coords_css$y
+      
+      # create style property for tooltip
+      # background color is set so tooltip is a bit transparent
+      # z-index is set so we are sure are tooltip will be on top
+      style <- paste0(
+        "position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); ",
+        "left:", left_px, "px; top:", top_px, "px;"
+      )
+      
+      # actual tooltip created as wellPanel
+      wellPanel(
+        style = style,
+        strong(point$gene)
+      )
+    })
     
     
   })
